71ace85fd7fad710adaa5fbfbe66d1cb8b1f459a
Atziri Mancilla <atzirimc22@gmail.com>
2024-03-30 21:30:12 -0600

    cambios clase date

--- src/Date.java
+++ src/Date.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Date {
     private int dayOfMonth;
     private int month;
@@ -31,4 +33,33 @@ public class Date {
     public void setYear(int year) {
         this.year = year;
     }
+    public void modifyDate(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Ingrese la fecha de Nacimiento: ");
+        System.out.println("Año: ");
+        int newYear = sc.nextInt();
+        if(newYear>1900&&newYear<2024)
+            year=newYear;
+        else{
+            System.out.println("Año no valido");
+        }
+        System.out.println("Mes: ");
+        int newMonth = sc.nextInt();
+        if(newMonth<13&&newMonth>0)
+            month=newMonth;
+        else{
+            System.out.println("Mes no valido");
+        }
+        System.out.println("Dia: ");
+        int newDayOfMonth = sc.nextInt();
+        if(newDayOfMonth>0&&newDayOfMonth<32)
+            dayOfMonth=newDayOfMonth;
+        else{
+            System.out.println("dia no valido");
+        }
+    }
+    public String showDate(){
+        return Integer.toString(dayOfMonth) + "/" + Integer.toString(month) + "/" + Integer.toString(year);
+    }
 }
+
